9169a51d0a79f080cbf9b810bbd1f99e7d527542
mizuki_r <ry.mizuki@gmail.com>
2024-06-09 09:28:27 +0900

    chore(eslint): fix settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,8 @@ module.exports = {
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2023,
-    project: ['./base.json'],
-    tsconfigRootDir: path.resolve(__dirname, '../tsconfig')
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname
   },
   plugins: ['@typescript-eslint'],
   extends: [
